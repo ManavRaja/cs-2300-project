6043237b5fd49be4f801ceb6746ac313ceba62bc
3unic3 <eunice.suhaana@gmail.com>
2025-05-02 10:55:14 -0500

    all recipes

--- recipe_site/main_app/views.py
+++ recipe_site/main_app/views.py
@@ -235,3 +235,10 @@ def create_recipe_view(request):
     }
     # Render the template used for creating recipes.
     return render(request, 'main_app/create_recipe.html', context)
+
+from django.shortcuts import render
+from .models import CustomRecipe  # make sure your model is imported
+
+def all_recipes(request):
+    recipes = CustomRecipe.objects.all()
+    return render(request, 'main_app/all_recipes.html', {'recipes': recipes})
